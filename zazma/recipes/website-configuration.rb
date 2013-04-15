6b3e4103f38ab64b52a6cbea7da21c04a342cf17
idan <idan@zazma.com>
2013-04-15 14:12:23 +0300

    asd

--- zazma/recipes/website-configuration.rb
+++ zazma/recipes/website-configuration.rb
@@ -1,4 +1,18 @@
-template '/var/website/configuration/#{node[:deploy][:website][:zazma][:configuration]}' do
+directory "/var/website/configuration/" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+directory "/var/website/logs/" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+template "/var/website/configuration/#{node[:deploy][:website][:zazma][:configuration]}" do
   path "/var/website/configuration/#{node[:deploy][:website][:zazma][:configuration]}.php"
   source 'configuration_template.php'
   owner 'root'
