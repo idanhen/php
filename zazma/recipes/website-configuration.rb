f3374dd858eb4c9ac76c9adf039557e77dd5616d
idan <idan@zazma.com>
2013-04-17 12:00:24 +0300

    asd

--- zazma/recipes/website-configuration.rb
+++ zazma/recipes/website-configuration.rb
@@ -21,7 +21,7 @@ end
 
 template "/var/website/configuration/#{node[:deploy][:website][:zazma][:configuration]}" do
   path "/var/website/configuration/#{node[:deploy][:website][:zazma][:configuration]}.php"
-  source 'configuration_template.php'
+  source 'configuration_template.php.erb'
   owner 'root'
   group 'root'
   mode 0644
